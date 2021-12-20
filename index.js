c1c4464d0bf1ef027cb9b76b747465c8df00d4fa
nongtokung <thesamurai1234@gmail.com>
2021-12-20 18:23:50 +0700

    2 files modified

--- index.js
+++ index.js
@@ -26,7 +26,83 @@ const friendShip = document.getElementById("friendShip")
 
 async function main() {
   // Initialize LIFF app)
-  
+  await liff.init({ liffId: "1656739806-L0G7m3K1" })
+
   // Try a LIFF function
+  switch (liff.getOS()) {
+    case "android": body.style.backgroundColor = "#d1f5d3"; break
+    case "ios": body.style.backgroundColor = "#4287f5"; break
+  }
+
+
+  if (!liff.isInClient()) {
+    if (liff.isLoggedIn()) {
+      btnLogIn.style.display = "none"
+      btnLogOut.style.display = "block"
+      getUserProfile()
+      getFriendship()
+    } else {
+      btnLogIn.style.display = "block"
+      btnLogOut.style.display = "none"
+    }
+  }else {
+    btnSend.style.display = "block"
+    getUserProfile()
+    getFriendship()
+  }
+
+  btnOpenWindow.style.display = "block"
 }
 main()
+
+async function getUserProfile() {
+  const profile = await liff.getProfile()
+  pictureUrl.src = profile.pictureUrl
+  userId.innerHTML = "<b>userId:</b> " + profile.userId
+  statusMessage.innerHTML = "<b>statusMessage:</b> " + profile.statusMessage
+  displayName.innerHTML = "<b>displayName:</b> " + profile.displayName
+
+  email.innerHTML = "<b>email:</b> " + liff.getDecodedIDToken().email
+}
+
+btnLogIn.onclick = () => {
+  liff.login()
+}
+
+btnLogOut.onclick = () => {
+  liff.logout()
+  window.location.reload()
+}
+
+async function sendMsg() {
+  if (liff.getContext().type !== "none" && liff.getContext().type !== "external") {
+    await liff.sendMessages([
+      {
+        "type": "text",
+        "text": "This message was sent by sendMessages()"
+      }
+    ])
+    //alert("Message sent")
+    liff.closeWindow()
+  }
+}
+
+btnSend.onclick = () => {
+  sendMsg()
+}
+
+btnOpenWindow.onclick = () => {
+  liff.openWindow({
+    url: window.location.href,
+    external: true
+  })
+}
+
+async function getFriendship() {
+  let msg = "Hooray! You and our chatbot are friend."
+  const friend = await liff.getFriendship()
+  if (!friend.friendFlag) {
+     msg = "<a href=\"https://line.me/R/ti/p/@085ovuzv/botrouy\">Follow our chatbot here!</a>"
+  }
+  friendShip.innerHTML = msg;
+}
